ba00bcbfd54e557d3c4b5bb702063af3c4ee3c88
Farha B. Jawed <farhajw@gmail.com>
2018-05-28 16:56:05 -0700

    cosmetics

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-
+//Local storage
 if(typeof(Storage) !== "undefined") {
     if(localStorage.mov){
         var retrievedData = localStorage.mov;
@@ -6,19 +6,18 @@ if(typeof(Storage) !== "undefined") {
         movies = JSON.parse(retrievedData);
     }
     else{
-        console.log("not stored");
-        var movies=["13 Reasons why","Wall-E","Breaking Bad","A Walk to Remember","Life is Beautiful","Friends","How I met your mother"];
+        var movies=["13 Reasons why","Gladiator","Wall-E","Breaking Bad","Better Call Saul","A Walk to Remember","Life is Beautiful","Friends","How I met your mother"];
         // convert to string
         localStorage.setItem("mov", JSON.stringify(movies));
     }
 }
-var genericMovie =[];
+var lowercaseMovie =[];
 
 
 
 function convertToLowerCase(){
     movies.forEach(function(element,i){
-       genericMovie[i]=element.toLowerCase();
+        lowercaseMovie[i]=element.toLowerCase();
     })
 }
 
@@ -37,11 +36,13 @@ function renderMovieButtons(){
 function displayGif(){
 
     console.log($(this));
+   
+    // active button
     $(".movie-btn").removeClass("color-change");
     $(this).addClass("color-change");
+
     var movie = $(this).attr("data-movie");
   
-
     var apiKey = "ahF05f6KjoiQhSOZtwg8yCLLjNtuRdun";
     var limit = 10;
     var queryURL = "https://api.giphy.com/v1/gifs/search?q="+movie+"&api_key="+apiKey+"&limit="+limit;
@@ -53,8 +54,6 @@ function displayGif(){
         }).then(function(response){
 
             $("#movies-view").empty();
-        
-
             console.log(response);    
             for(var i = 0; i < limit;i++){
                 var movieDiv = $("<div>");
@@ -78,8 +77,7 @@ function displayGif(){
                 hImageStill.addClass("gif img-fluid");
                 var hCaption = $("<div>").addClass("caption text-center").text("Rating: ");
                 var hRate = $("<span>").text(rating);
-                hCaption.append(hRate);
-                
+                hCaption.append(hRate);   
                 movieDiv.append(hImageStill,hCaption);
                 $("#movies-view").append(movieDiv);
             }
@@ -89,20 +87,23 @@ function displayGif(){
 
 renderMovieButtons();
 
-
+//display gif on movie-button click
 $("#buttons-view").on("click",".movie-btn",displayGif);
 
+//add movie buttons
 $("#add-movie").on("click",function(event){
     event.preventDefault();
     convertToLowerCase();
     var movie = $("#movie-input").val().trim();
-    if(movie!=='' && genericMovie.indexOf(movie.toLowerCase())===-1){
+    //exclude empty strings and duplicates
+    if(movie!=='' && lowercaseMovie.indexOf(movie.toLowerCase())===-1){
         movies.push(movie);
         localStorage.setItem("mov", JSON.stringify(movies));
         renderMovieButtons();
     }   
 })
 
+//animate and pause on mouseenter
 $("#movies-view").on("mouseenter", ".gif", function() {
     var state = $(this).attr("data-state");
     if (state === "still") {
@@ -114,15 +115,12 @@ $("#movies-view").on("mouseenter", ".gif", function() {
     }
   });
 
-
+//modal on click with exapanded view
 $("#movies-view").on("click", ".gif", function() {
-
     $(this).attr("src", $(this).attr("data-still"));
     $(this).attr("data-state", "still");
-   
     var imgLarge = $("<img>").attr("src",$(this).attr("data-large"));
     imgLarge.addClass("gifLarge");
     $(".modal-body").html(imgLarge);
     $(".modal-title").html($(this).attr("data-title"));
-
 });
