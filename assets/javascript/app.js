3525918afd88a0a968481389090e753811fab4df
Farha B. Jawed <farhajw@gmail.com>
2018-05-28 22:32:28 -0700

    2nd Api added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+$(".movie-info").hide();
+
 //Local storage
 if(typeof(Storage) !== "undefined") {
     if(localStorage.mov){
@@ -6,7 +8,7 @@ if(typeof(Storage) !== "undefined") {
         movies = JSON.parse(retrievedData);
     }
     else{
-        var movies=["13 Reasons why","Gladiator","Wall-E","Breaking Bad","Better Call Saul","A Walk to Remember","Life is Beautiful","Friends","How I met your mother"];
+        var movies=["13 Reasons why","Love Actually","Wall-E","Breaking Bad","Better Call Saul","A Walk to Remember","Life is Beautiful","Friends","How I met your mother"];
         // convert to string
         localStorage.setItem("mov", JSON.stringify(movies));
     }
@@ -34,54 +36,98 @@ function renderMovieButtons(){
 }
 
 function displayGif(){
-
-    console.log($(this));
    
     // active button
     $(".movie-btn").removeClass("color-change");
+    $("#movies-view").empty();
+    $(".gif-header").empty();
     $(this).addClass("color-change");
 
+    // 1st API: OMDP API FOR MOVIE INFO
     var movie = $(this).attr("data-movie");
-  
-    var apiKey = "ahF05f6KjoiQhSOZtwg8yCLLjNtuRdun";
-    var limit = 10;
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+movie+"&api_key="+apiKey+"&limit="+limit;
-    console.log(queryURL);
+    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
-        $.ajax({
-        url:queryURL,
-        method:"GET"
-        }).then(function(response){
-
-            $("#movies-view").empty();
-            console.log(response);    
-            for(var i = 0; i < limit;i++){
-                var movieDiv = $("<div>");
-                movieDiv.addClass("movie img-thumbnail");
-                var imageSmallStill = response.data[i].images.fixed_height_still.url;
-                var imageSmall = response.data[i].images.fixed_height.url;
-                var imageLarge = response.data[i].images.downsized_large.url;
-                var title = response.data[i].title;
-                var rating = response.data[i].rating;
-                var hTitle = $("<p>").text(title);
-                var hImageStill = $("<img>").attr({
-                    "src":imageSmallStill,
-                    "data-state":"still",
-                    "data-animate":imageSmall,
-                    "data-still":imageSmallStill,
-                    "data-large":imageLarge,
-                    "data-title":title,
-                    "data-toggle":"modal",
-                    "data-target":".exampleGif"
-                });
-                hImageStill.addClass("gif img-fluid");
-                var hCaption = $("<div>").addClass("caption text-center").text("Rating: ");
-                var hRate = $("<span>").text(rating);
-                hCaption.append(hRate);   
-                movieDiv.append(hImageStill,hCaption);
-                $("#movies-view").append(movieDiv);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(data) {
+            //Error handling if movie not found
+            if(data.Response==="False"){
+                $(".movie-info").hide();
+                var div = $("<div class='alert alert-danger'>").html("<strong>"+data.Error+"</strong>");
+                $("#movies-view").html(div);
             }
-        });  
+            else{
+                var title = data.Title;
+                var poster = data.Poster;
+                var actors = data.Actors;
+                var plot = data.Plot;
+                var genre = data.Genre;
+                var imdbRating = data.imdbRating;
+                var released = data.Released;
+                console.log(title,poster,actors,plot,imdbRating,released);
+              
+                $("#title").html('"'+title+'"');
+                var cardPoster = $("<img>").attr("src",poster);
+                $("#poster").html(cardPoster);
+                $("#actors").html("<span>Actors:</span> "+actors);
+                $("#genre").html("<span>Genre:</span> "+genre);
+                $("#released").html("<span>Release Date:</span> "+released);
+                $("#imdbRating").html("<span>IMBD Rating:</span> "+imdbRating);
+                $("#plot").html("<span>Plot:</span> "+plot);
+                $(".movie-info").show();
+
+
+                //2nd API: GIPHY API
+                var apiKey = "ahF05f6KjoiQhSOZtwg8yCLLjNtuRdun";
+                var limit = 10;
+                var queryURL = "https://api.giphy.com/v1/gifs/search?q="+movie+"&api_key="+apiKey+"&limit="+limit;
+                console.log(queryURL);
+
+                    $.ajax({
+                    url:queryURL,
+                    method:"GET"
+                    }).then(function(response){
+                      //if GIF found
+                      if(response.data.length>0){
+                            var gifHeader = $("<div class='alert alert-info text-center'>").html("GIFs for Search Term : "+movie);
+                            $(".gif-header").append(gifHeader);
+
+                            for(var i = 0; i < limit;i++){
+                                var movieDiv = $("<div>");
+                                movieDiv.addClass("movie img-thumbnail");
+                                var imageSmallStill = response.data[i].images.fixed_height_still.url;
+                                var imageSmall = response.data[i].images.fixed_height.url;
+                                var imageLarge = response.data[i].images.downsized_large.url;
+                                var title = response.data[i].title;
+                                var rating = response.data[i].rating;
+                                var hTitle = $("<p>").text(title);
+                                var hImageStill = $("<img>").attr({
+                                    "src":imageSmallStill,
+                                    "data-state":"still",
+                                    "data-animate":imageSmall,
+                                    "data-still":imageSmallStill,
+                                    "data-large":imageLarge,
+                                    "data-title":title,
+                                    "data-toggle":"modal",
+                                    "data-target":".exampleGif"
+                                });
+                                hImageStill.addClass("gif img-fluid");
+                                var hCaption = $("<div>").addClass("caption text-center").text("Rating: ");
+                                var hRate = $("<span>").text(rating);
+                                hCaption.append(hRate);   
+                                movieDiv.append(hImageStill,hCaption);
+                                $("#movies-view").append(movieDiv);
+                            }
+                        }
+                        //else show error message
+                        else{
+                            var gifHeader = $("<div class='alert alert-danger text-center'>").html("<strong>No Gifs found! :( </strong>");
+                            $(".gif-header").append(gifHeader);
+                        }
+                    });  
+                }
+            });
 }
 
 
@@ -117,8 +163,12 @@ $("#movies-view").on("mouseenter", ".gif", function() {
 
 //modal on click with exapanded view
 $("#movies-view").on("click", ".gif", function() {
-    $(this).attr("src", $(this).attr("data-still"));
-    $(this).attr("data-state", "still");
+    //pause all gifs when expanded
+    $(".gif").each(function() {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    });
+
     var imgLarge = $("<img>").attr("src",$(this).attr("data-large"));
     imgLarge.addClass("gifLarge");
     $(".modal-body").html(imgLarge);
